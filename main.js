48b8d4959c11c1e498f8d26653f88848f7fca7b6
lazarkulasevic <lazar.kulasevic@gmail.com>
2020-09-19 00:05:15 +0200

    work in progress

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
 const bodyElement = document.body;
 const time = new Date().getHours();
 const inputSwitch = document.querySelector('input[name=dark-mode]');
-const imgMountain = document.getElementById('mountain');
+const imgMountain = document.querySelectorAll('.mountain');
 
 darkMode(time);
 
 inputSwitch.addEventListener('click', () => {
     bodyElement.classList.toggle('dark-mode');
-    imgMountain.classList.toggle('dark-filter');
+    imgMountain[0].classList.toggle('dark-filter');
+    imgMountain[1].classList.toggle('dark-filter');
 });
 
 function darkMode(time) {
     if (time > 20 || time < 6) {
-        bodyElement.classList.add('dark-mode');
-        imgMountain.classList.add('dark-filter');
         inputSwitch.checked = true;
+        bodyElement.classList.add('dark-mode');
+        imgMountain[0].classList.add('dark-filter');
+        imgMountain[1].classList.add('dark-filter');
     } else {
-        bodyElement.classList.remove('dark-mode');
-        imgMountain.classList.remove('dark-filter');
         inputSwitch.checked = false;
+        bodyElement.classList.remove('dark-mode');
+        imgMountain[0].classList.remove('dark-filter');
+        imgMountain[1].classList.remove('dark-filter');
     }
 }
-
 
 
 
