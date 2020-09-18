1895bcd0fb2219179d3bd2d6e82eb673e21c3949
lazarkulasevic <lazar.kulasevic@gmail.com>
2020-09-18 12:59:17 +0200

    working on parallax

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
 const bodyElement = document.body;
 const time = new Date().getHours();
 const inputSwitch = document.querySelector('input[name=dark-mode]');
+const imgMountain = document.getElementById('mountain');
 
 darkMode(time);
 
 inputSwitch.addEventListener('click', () => {
-    bodyElement.classList.toggle('dark-mode')
+    bodyElement.classList.toggle('dark-mode');
+    imgMountain.classList.toggle('dark-filter');
 });
 
 function darkMode(time) {
     if (time > 20 || time < 6) {
         bodyElement.classList.add('dark-mode');
+        imgMountain.classList.add('dark-filter');
         inputSwitch.checked = true;
     } else {
         bodyElement.classList.remove('dark-mode');
+        imgMountain.classList.remove('dark-filter');
         inputSwitch.checked = false;
     }
 }
