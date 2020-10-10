0faba88b7e8f0530819f8c2d580fe793a1de7686
lazarkulasevic <lazar.kulasevic@gmail.com>
2020-10-10 15:22:57 +0200

    skills update

--- main.js
+++ main.js
@@ -1,44 +1,47 @@
 const bodyElement = document.body;
 const date = new Date();
 const time = date.getHours();
-const inputSwitch = document.querySelector('input[name=dark-mode]');
-const itemTitleAll = document.querySelectorAll('.item-title');
-const itemImgAll = document.querySelectorAll('.item img');
+const inputSwitch = document.querySelector("input[name=dark-mode]");
+const itemTitleAll = document.querySelectorAll(".item-title");
+const itemImgAll = document.querySelectorAll(".item img");
 
-const divSkills = document.querySelector('.skills');
-const lastSkill = document.querySelector('.lastSkill');
-const spanCountdown = document.querySelector('.countdown');
-const faClock = document.querySelector('.fa-clock');
+const divSkills = document.querySelector(".skills");
+const lastSkill = document.querySelector(".lastSkill");
+const spanCountdown = document.querySelector(".countdown");
+const faClock = document.querySelector(".fa-clock");
 
 darkMode(time);
 
-inputSwitch.addEventListener('click', () => {
-    bodyElement.classList.toggle('dark-mode');
+inputSwitch.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark-mode");
 });
 
 function darkMode(time) {
-    if (time > 20 || time < 6) {
-        inputSwitch.checked = true;
-        bodyElement.classList.add('dark-mode');
-    } else {
-        inputSwitch.checked = false;
-        bodyElement.classList.remove('dark-mode');
-    }
+  if (time > 20 || time < 6) {
+    inputSwitch.checked = true;
+    bodyElement.classList.add("dark-mode");
+  } else {
+    inputSwitch.checked = false;
+    bodyElement.classList.remove("dark-mode");
+  }
 }
 
 // PHONE & TOUCH
-if ('ontouchstart' in bodyElement) {
-    itemTitleAll.forEach(itemTitle => {
-        itemTitle.style.visibility = "visible";
-        itemTitle.style.backgroundColor = "#0000007c";
-    })
-    itemImgAll.forEach(img => {
-        img.style.animation = "filter-animation 5s infinite";
-    })
-    clockMouseEvent('touchstart', 'touchend');
+if ("ontouchstart" in bodyElement) {
+  itemTitleAll.forEach((itemTitle) => {
+    itemTitle.style.visibility = "visible";
+    itemTitle.style.backgroundColor = "#0000007c";
+  });
+  itemImgAll.forEach((img) => {
+    img.style.animation = "filter-animation 5s infinite";
+  });
+  clockMouseEvent("touchstart", "touchend");
 }
 
+// REUSABLE CODE SNIPPET
+/*
 // Countdown to new skill
+
 let dueDate = new Date(2020, 9, 10).getTime();
 let dateTime = date.getTime();
 
@@ -107,3 +110,5 @@ function clockMouseEvent(eventOn, eventOff) {
         }, 400);
     });
 }
+
+*/
