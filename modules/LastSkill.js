3d4a3086b67f51b409743fb1f2bc23d5c62ebe0c
lazarkulasevic <lazar.kulasevic@gmail.com>
2020-11-14 21:56:27 +0100

    indent 2 spaces

--- modules/LastSkill.js
+++ modules/LastSkill.js
@@ -5,65 +5,65 @@ const spanCountdown = document.querySelector(".countdown");
 const faClock = document.querySelector(".fa-clock");
 
 function countdown(dateTime, dueDate) {
-    let daysLeft = Math.floor((dueDate - dateTime) / 1000 / 60 / 60 / 24);
-    let weeksLeft = (daysLeft - (daysLeft % 7)) / 7;
-    let text;
+	let daysLeft = Math.floor((dueDate - dateTime) / 1000 / 60 / 60 / 24);
+	let weeksLeft = (daysLeft - (daysLeft % 7)) / 7;
+	let text;
 
-    daysLeft = (daysLeft % 7) > 1 ? (daysLeft % 7) + ' days' :
-        (daysLeft % 7) == 1 ? (daysLeft % 7) + ' day' : '';
+	daysLeft = (daysLeft % 7) > 1 ? (daysLeft % 7) + ' days' :
+		(daysLeft % 7) == 1 ? (daysLeft % 7) + ' day' : '';
 
-    weeksLeft = weeksLeft > 1 ? weeksLeft + ' weeks' :
-        weeksLeft == 1 ? weeksLeft + ' week' : '';
+	weeksLeft = weeksLeft > 1 ? weeksLeft + ' weeks' :
+		weeksLeft == 1 ? weeksLeft + ' week' : '';
 
-    if (daysLeft && weeksLeft) {
-        text = document.createTextNode(
-            `Approximately ${weeksLeft} and ${daysLeft} left to learn Vue basics.`
-        );
-    } else if (!daysLeft && weeksLeft) {
-        text = document.createTextNode(
-            `Approximately ${weeksLeft} left to learn Vue basics.`
-        );
-    } else if (daysLeft && !weeksLeft) {
-        text = document.createTextNode(
-            `Approximately ${daysLeft} left to learn Vue basics.`
-        );
-    } else {
-        text = document.createTextNode(
-            `I have learned VueJS basics.`
-        );
-        lastSkill.textContent = 'VueJS (basics)';
-    }
-    spanCountdown.appendChild(text);
+	if (daysLeft && weeksLeft) {
+		text = document.createTextNode(
+			`Approximately ${weeksLeft} and ${daysLeft} left to learn Vue basics.`
+		);
+	} else if (!daysLeft && weeksLeft) {
+		text = document.createTextNode(
+			`Approximately ${weeksLeft} left to learn Vue basics.`
+		);
+	} else if (daysLeft && !weeksLeft) {
+		text = document.createTextNode(
+			`Approximately ${daysLeft} left to learn Vue basics.`
+		);
+	} else {
+		text = document.createTextNode(
+			`I have learned VueJS basics.`
+		);
+		lastSkill.textContent = 'VueJS (basics)';
+	}
+	spanCountdown.appendChild(text);
 }
 
 function clockMouseEvent(eventOn, eventOff) {
-    let clicked = false;
-    faClock.addEventListener(eventOn, () => {
-        if (eventOn == 'touchstart' && !clicked) {
-            clicked = true;
+	let clicked = false;
+	faClock.addEventListener(eventOn, () => {
+		if (eventOn == 'touchstart' && !clicked) {
+			clicked = true;
 
-            let divAlert = document.createElement('div');
-            let countdownText = document.createTextNode(spanCountdown.textContent);
-            divAlert.appendChild(countdownText);
-            divAlert.setAttribute('class', 'divAlert');
-            divSkills.appendChild(divAlert);
+			let divAlert = document.createElement('div');
+			let countdownText = document.createTextNode(spanCountdown.textContent);
+			divAlert.appendChild(countdownText);
+			divAlert.setAttribute('class', 'divAlert');
+			divSkills.appendChild(divAlert);
 
-            setTimeout(() => {
-                divSkills.removeChild(divSkills.lastChild);
-                clicked = false;
-            }, 3000);
-        }
-        if (document.body.classList.contains('dark-mode')) {
-            spanCountdown.style.color = '#ffe605';
-            return;
-        }
-        spanCountdown.style.color = '#4f46fd';
-    });
-    faClock.addEventListener(eventOff, () => {
-        setTimeout(() => {
-            spanCountdown.style.color = '';
-        }, 400);
-    });
+			setTimeout(() => {
+				divSkills.removeChild(divSkills.lastChild);
+				clicked = false;
+			}, 3000);
+		}
+		if (document.body.classList.contains('dark-mode')) {
+			spanCountdown.style.color = '#ffe605';
+			return;
+		}
+		spanCountdown.style.color = '#4f46fd';
+	});
+	faClock.addEventListener(eventOff, () => {
+		setTimeout(() => {
+			spanCountdown.style.color = '';
+		}, 400);
+	});
 }
 
 export {countdown, clockMouseEvent};
