3b4c7988983af8a7ca6764e4f8450194e284d2be
lazarkulasevic <lazar.kulasevic@gmail.com>
2020-11-15 15:55:36 +0100

    rearange some code

--- main.js
+++ main.js
@@ -44,10 +44,10 @@ countdown(dateTime, dueDate);
 clockMouseEvent('mouseenter', 'mouseleave');
 // ##################################################
 
-
 const portfolio = new PortfolioItem(divPortfolio);
 
 portfolio.item('Memory Game', 'images/memory-game-screen.png', 'https://lazarkulasevic.github.io/memory-game/');
 portfolio.item('Guess The Number', 'images/binary-search-featured.png', 'https://lazarkulasevic.github.io/binary-search/');
 portfolio.item('Public Chat', 'images/chat-screen.png', 'https://lazars-chat.web.app/');
 portfolio.item('To-do List (React)', 'images/ReactToDo.png', 'https://lazarkulasevic.github.io/react-to-do/');
+
