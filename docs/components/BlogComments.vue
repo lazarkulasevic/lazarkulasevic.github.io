f6099979d41d18a47d736afa53a0738e21aa944c
lazar.kulasevic <lazar.kulasevic@gmail.com>
2022-08-10 10:56:30 +0200

    Added giscus comments

--- docs/components/BlogComments.vue
+++ docs/components/BlogComments.vue
@@ -15,7 +15,7 @@ onMounted(() => {
         'data-category': 'General',
         'data-category-id': 'DIC_kwDOEZVcTs4CQt8M',
         'data-mapping': 'pathname',
-        'data-strict': '1',
+        'data-strict': '0',
         'data-reactions-enabled': '1',
         'data-emit-metadata': '0',
         'data-input-position': 'top',
