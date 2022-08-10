9161027356dfba15ffdfc22d51296e8af5eae773
lazar.kulasevic <lazar.kulasevic@gmail.com>
2022-08-10 11:31:25 +0200

    Added giscus comments

--- docs/components/BlogComments.vue
+++ docs/components/BlogComments.vue
@@ -11,7 +11,7 @@ onMounted(() => {
         async: true,
         'src': 'https://giscus.app/client.js',
         'data-repo': 'lazarkulasevic/lazarkulasevic.github.io',
-        'data-repo-id': 'lMDEwOlJlcG9zaXRvcnkyOTUwMDExNjY=',
+        'data-repo-id': 'MDEwOlJlcG9zaXRvcnkyOTUwMDExNjY=',
         'data-category': 'General',
         'data-category-id': 'DIC_kwDOEZVcTs4CQt8M',
         'data-mapping': 'pathname',
