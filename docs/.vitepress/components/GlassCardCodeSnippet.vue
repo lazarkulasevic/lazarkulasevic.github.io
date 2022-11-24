2b8e36f7ba9919fb2d0407590ecf6bdba22a0b08
lazarkulasevic <lazar.kulasevic@gmail.com>
2022-11-24 14:08:43 +0100

    css fix

--- docs/.vitepress/components/GlassCardCodeSnippet.vue
+++ docs/.vitepress/components/GlassCardCodeSnippet.vue
@@ -1,5 +1,5 @@
 <template>
-    <div data-v-95b970be="" style="position: relative;"><div><div class="language-js"><span class="copy"></span><pre><code><span class="line"><span style="font-style:italic;" class="code--blue">while</span><span class="code--purple"> (working) </span><span class="code--blue">{</span></span>
+    <div data-v-95b970be="" style="position: relative;font-size: 16px;"><div><div class="language-js"><span class="copy"></span><pre><code><span class="line"><span style="font-style:italic;" class="code--blue">while</span><span class="code--purple"> (working) </span><span class="code--blue">{</span></span>
 <span class="line"><span>    </span><span class="code--purple">coffee</span><span class="code--blue">.</span><span class="code--green">drink</span><span class="code--purple">()</span></span>
 <span class="line"><span>    </span><span class="code--purple">work</span><span class="code--blue">.</span><span class="code--green">execute</span><span class="code--purple">()</span></span>
 <span class="line"><span>    </span><span style="font-style:italic;" class="code--blue">if</span><span class="code--purple"> (</span><span class="code--purple">coffee</span><span class="code--purple"> </span><span class="code--blue">===</span><span class="code--purple"> </span><span class="code--blue">'</span><span class="code--yellow">empty</span><span class="code--blue">'</span><span class="code--purple">) </span><span class="code--blue">{</span></span>
@@ -9,7 +9,7 @@
 <span class="line"></span></code></pre></div></div></div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @use '../style/variables' as *;
 
 .code {
